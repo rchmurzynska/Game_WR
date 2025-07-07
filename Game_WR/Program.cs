425fb7e1313927e61e4e1bf2122ea67be4dfca3b
Reese Cloud <roksana.chmurzynska@gmail.com>
2025-07-07 13:35:33 +0200

    lvl change

--- Game_WR/Program.cs
+++ Game_WR/Program.cs
@@ -2,6 +2,15 @@
 
 Console.CursorVisible = false;
 
+Dictionary<ConsoleKey, string> keyActionMap = new Dictionary<ConsoleKey, string>();
+keyActionMap.Add(ConsoleKey.A, "moveLeft");
+keyActionMap.Add(ConsoleKey.D, "moveRight"); 
+keyActionMap.Add(ConsoleKey.W, "moveUp");
+keyActionMap.Add(ConsoleKey.S, "moveDown");
+keyActionMap.Add(ConsoleKey.Escape, "exit");
+keyActionMap.Add(ConsoleKey.F, "attack");
+
+
 Dictionary<string, Point> directionsMap = new Dictionary<string, Point>();
 directionsMap.Add("moveLeft", new Point(-1, 0));
 directionsMap.Add("moveRight", new Point(1, 0));
@@ -14,4 +23,9 @@ hero.speed = 1;
 hero.position = startingPoint;
 
 List<Character> characters = new List<Character>();
-characters.Add(hero);
+Level FirstLevel = new Level (@"C:\Users\roksa\Desktop\Game_WR\Game_WR\Levels\FirstLevel.txt");
+
+Level currentLevel = FirstLevel;
+Player hero = new Player("Star", "S", startingPoint, currentLevel, keyActionMap);
+
+
